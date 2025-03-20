a1d055c4587880f2420593f3ef8d106b0262d1f9
unknown <bautista.dondon.c210220@gmail.com>
2025-03-20 16:16:28 -0700

    Fixed Railway PORT and MySQL connection

--- config/db.js
+++ config/db.js
@@ -1,21 +1,41 @@
-const mysql = require('mysql2/promise'); // Import mysql2 with promise support
+// db.js (use MYSQL_URL on Railway)
+const mysql = require('mysql2/promise');
+const { URL } = require('url');
+require('dotenv').config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'sibbap_management_system',
-  connectionLimit: 10 // You can adjust this limit as needed
-});
+let dbConfig;
 
-// Optionally test the connection pool
+if (process.env.MYSQL_URL) {
+  const dbUrl = new URL(process.env.MYSQL_URL);
+  dbConfig = {
+    host: dbUrl.hostname,
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.replace('/', ''),
+    port: dbUrl.port,
+    connectionLimit: 10
+  };
+} else {
+  // Local Development
+  dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'sibbap_management_system',
+    port: process.env.DB_PORT || 3306,
+    connectionLimit: 10
+  };
+}
+
+const db = mysql.createPool(dbConfig);
+
 db.getConnection()
   .then(connection => {
     console.log('Connected to the MySQL database.');
-    connection.release(); // Release the connection back to the pool
+    connection.release();
   })
   .catch(err => {
     console.error('Error connecting to the database:', err);
   });
 
-module.exports = db; // Export the pool for use in other files
+module.exports = db;
