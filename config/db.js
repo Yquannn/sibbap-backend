82f7e3760b37ed9da81c864be5429b634b2663fc
unknown <bautista.dondon.c210220@gmail.com>
2025-03-20 16:23:32 -0700

    test

--- config/db.js
+++ config/db.js
@@ -1,41 +1,15 @@
-// db.js (use MYSQL_URL on Railway)
 const mysql = require('mysql2/promise');
 const { URL } = require('url');
-require('dotenv').config();
 
-let dbConfig;
+const dbUrl = new URL(process.env.MYSQL_URL);
 
-if (process.env.MYSQL_URL) {
-  const dbUrl = new URL(process.env.MYSQL_URL);
-  dbConfig = {
-    host: dbUrl.hostname,
-    user: dbUrl.username,
-    password: dbUrl.password,
-    database: dbUrl.pathname.replace('/', ''),
-    port: dbUrl.port,
-    connectionLimit: 10
-  };
-} else {
-  // Local Development
-  dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'sibbap_management_system',
-    port: process.env.DB_PORT || 3306,
-    connectionLimit: 10
-  };
-}
-
-const db = mysql.createPool(dbConfig);
-
-db.getConnection()
-  .then(connection => {
-    console.log('Connected to the MySQL database.');
-    connection.release();
-  })
-  .catch(err => {
-    console.error('Error connecting to the database:', err);
-  });
+const db = mysql.createPool({
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace('/', ''),
+  port: dbUrl.port,
+  connectionLimit: 10
+});
 
 module.exports = db;
